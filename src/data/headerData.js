fc3539547d7c0269e554b3b421e35fd2cf7a470e
jonnyprem <jonnyprem007@gmail.com>
2023-02-12 10:17:09 +0700

    Update Photo

--- src/data/headerData.js
+++ src/data/headerData.js
@@ -1,9 +1,10 @@
 import resume from '../assets/pdf/resume.pdf'
+import profile from '../assets/pdf/profile.jpg'
 
 export const headerData = {
-    name: 'Jonny Prem',
+    name: 'Leapheng PRME',
     title: "Data Scientist",
     desciption:"An enthusiastic Data Geek who has the ability to effectively find and interpret solutions for key audiences and businesses. With a background in iOS mobile application development and website building and a bachelor's in Information Technology, I am passionate about coming up with creative solutions in order to enable organizations to perform better and make smarter business decisions with a data-driven approach. This is also paired with a curious data mindset, and the ability to ask the right type of questions and apply logic to any business challenge, no matter how daunting it might seem.",
-    image: 'https://z-p3-scontent.fpnh5-2.fna.fbcdn.net/v/t39.30808-6/304805506_3309341729387821_1395053391357020853_n.jpg?stp=cp6_dst-jpg_p526x296&_nc_cat=102&ccb=1-7&_nc_sid=8bfeb9&_nc_eui2=AeE6jgXvlIfhoby2nfuAx--XtjJKUKX5b--2MkpQpflv7xWWka9oEJytrw8fSD5x9cTvlc7KWPPC4W0MOAUu1lpo&_nc_ohc=kez2OmExjwgAX9QJS1p&tn=cfocdGUa8JOpWYgB&_nc_zt=23&_nc_ht=z-p3-scontent.fpnh5-2.fna&oh=00_AT_cO6kLmyqzHS9tr7oAUb5R5tVpq-ynGp-o0E1S23c2GQ&oe=6320A7FC',
+    image: profile,
     resumePdf: resume
 }
